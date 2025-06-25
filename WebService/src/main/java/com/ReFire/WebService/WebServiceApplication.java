96e870eeddd9f1cb27290ee639e50548817b56f4
Jonathan Francisco <Jon@Mac.lan>
2025-06-25 00:31:22 -0700

    added search page

--- WebService/src/main/java/com/ReFire/WebService/WebServiceApplication.java
+++ WebService/src/main/java/com/ReFire/WebService/WebServiceApplication.java
@@ -13,8 +13,9 @@ public class WebServiceApplication
     {
       SpringApplication.run(WebServiceApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+    @GetMapping("/search")
+    public String search()
+    {
+    	return "Search Results";
     }
 }
