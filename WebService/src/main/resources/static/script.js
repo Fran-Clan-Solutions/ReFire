ef22f43e2e38b9419ae27e508f8311bb91af1fbd
Jonathan Francisco <Jon@Mac.lan>
2025-07-04 02:20:21 -0700

    Updating frontend

--- WebService/src/main/resources/static/script.js
+++ WebService/src/main/resources/static/script.js
@@ -4,15 +4,23 @@ function addIngredient()
 
     $.ajax
     ({
-        url:"http://localhost:8080/addIngredient?ingredient=" + mIngredient
-    })
-    .then(function(data)
-    {
-        $("#ingredient_list").text(data);
+        url:"http://localhost:8080/addIngredient?ingredient=" + mIngredient,
+        success: function(res) {
+            var row_html_str = "<tr><td>" + res +"</td></tr>";
+    
+            if(res != "")
+            {
+                $("#ingredient_list").append(row_html_str);
+                
+                console.log(res + " appended");
+            }
+        }
     });
 }
 
 function search()
 {
     console.log("Searching...");
+    
+    console.log($("#ingredient_list"));
 }
