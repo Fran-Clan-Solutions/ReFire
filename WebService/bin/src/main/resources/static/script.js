7a7caa73d4f4ee38a54ad464d1f1db9602d2dea0
jdfrancisco <jdavidfrancisco@gmail.com>
2025-07-14 23:33:20 -0700

    Adding frontend whitelist for production and development deployment

---
+++ WebService/bin/src/main/resources/static/script.js
@@ -0,0 +1,36 @@
+let mIngredient_list = []
+
+function addIngredient()
+{
+    var mIngredient = $("#ingredient_input").val();
+
+    $.ajax
+    ({
+        url:"http://localhost:8080/addIngredient?ingredient=" + mIngredient,
+        success: function(res) 
+        {
+            var row_html_str = "<tr><td>" + res +"</td></tr>";
+    
+            if(res != "")
+            {
+                $("#ingredient_list").append(row_html_str);
+                const ingredient = mIngredient.trim();
+                if (ingredient) 
+                {
+                    mIngredient_list.push(ingredient);
+                    $("#ingredient_input").val(""); // clear input
+                }
+                console.log(res + " appended");
+            }
+        }
+    });
+}
+
+function search() 
+{
+    const params = new URLSearchParams();
+    mIngredient_list.forEach(i => params.append("ingredient_list", i));
+
+    // Navigate to recipes.html with query params
+    window.location.href = `recipes.html?${params.toString()}`;
+}
---
+++ WebService/src/main/java/com/ReFire/WebService/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.ReFire.WebService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        // list allowed frontend URLs below:
+                        .allowedOrigins(
+                            "http://52.53.173.137",  // Production Frontend
+                            "http://localhost:8080" // Development Frontend
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
