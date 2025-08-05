45c50889f2e895d38bbc66a390285add7967e783
jdfrancisco <jdavidfrancisco@gmail.com>
2025-08-05 00:57:46 -0700

    added logos updated so ingredients are bolded minor format fixes

--- WebService/src/main/resources/static/recipes.js
+++ WebService/src/main/resources/static/recipes.js
@@ -27,31 +27,40 @@ $(document).ready(function ()
     });
 });
 
-function renderRecipes()
-{
+function renderRecipes() {
+    const params = new URLSearchParams(window.location.search);
+    const userIngredients = params.getAll("ingredient_list").map(i => i.toLowerCase());
+
     const selectedFilters = $('.meal-filter:checked')
         .map(function () { return this.value.toUpperCase(); })
         .get();
 
     let filteredRecipes = allRecipes;
 
-    if (selectedFilters.length > 0)
-    {
+    // Filter by meal type
+    if (selectedFilters.length > 0) {
         filteredRecipes = allRecipes.filter(recipe =>
             selectedFilters.includes(recipe.mealType.toUpperCase()));
     }
 
+    // Sort: by number of matches (desc), then by cook time (asc)
+    filteredRecipes.sort((a, b) => {
+        const aMatches = a.ingredients.filter(ing => userIngredients.includes(ing.toLowerCase())).length;
+        const bMatches = b.ingredients.filter(ing => userIngredients.includes(ing.toLowerCase())).length;
+
+        if (bMatches !== aMatches) {
+            return bMatches - aMatches; // more matches first
+        }
+        return a.cookTime - b.cookTime; // shorter cook time first
+    });
+
     let html = "";
 
-    if (filteredRecipes.length === 0)
-    {
+    if (filteredRecipes.length === 0) {
         html = "<p>No recipes found.</p>";
-    }
-    else
-    {
+    } else {
         html = "<ul class='list-unstyled'>";
-        filteredRecipes.forEach((recipe, index) =>
-        {
+        filteredRecipes.forEach((recipe, index) => {
             html += `
                 <li class="mb-3 border-bottom pb-2">
                     <div class="recipe-header" style="cursor: pointer;" data-index="${index}">
@@ -65,23 +74,29 @@ function renderRecipes()
 
     $("#recipes_list").html(html);
 
-    $(".recipe-header").on("click", function ()
-    {
+    $(".recipe-header").on("click", function () {
         const index = $(this).data("index");
         const recipe = filteredRecipes[index];
 
+        // Highlight matching ingredients in blue
+        const highlightedIngredients = recipe.ingredients.map(ing => {
+            if (userIngredients.includes(ing.toLowerCase())) {
+                return `<span class="highlight-ingredient">${ing}</span>`;
+            }
+            return ing;
+        });
+
         $("#recipeDetailContent").html(`
-            <h4 class="text-danger">Re<span class="text-primary">cipe</span>: ${recipe.name}</h4>
+            <h4 class="text-danger">Recipe: <span class="text-primary">${recipe.name}</span></h4>
             <p><strong>Cook Time:</strong> ${recipe.cookTime} minutes</p>
-            <p><strong>Ingredients:</strong><br> ${recipe.ingredients.join(", ")}</p>
+            <p><strong>Ingredients:</strong><br> ${highlightedIngredients.join(", ")}</p>
             <p><strong>Instructions:</strong><br> ${recipe.instructions}</p>
         `);
 
         $("#recipeDetailPanel").addClass("open");
     });
 
-    $("#closePanel").on("click", function ()
-    {
+    $("#closePanel").on("click", function () {
         $("#recipeDetailPanel").removeClass("open");
     });
 }
